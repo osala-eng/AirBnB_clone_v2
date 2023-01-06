03fa0cae59157d882c53be613dc05203ae9b2956
Jashon Osala <jashonosala@gmail.com>
2023-01-06 21:08:17 +0300

    Update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from models.state import State
 from models.city import City
 from models.base_model import Base
-from models import stringtemplates as ENV
-import models
+from models import stringtemplates as ENV, classes
+
 
 
 class DBStorage:
@@ -30,18 +30,18 @@ class DBStorage:
         if env == ENV.TEST:
             Base.metadata.drop_all(self.__engine)
 
-    def all(self, cls=None):
+    def all(self, cls=None) -> dict:
         ''' Query current DB session '''
         database = {}
 
         if cls != '':
-            objs = self.__session.query(models.classes[cls]).all()
+            objs = self.__session.query(classes[cls]).all()
             for obj in objs:
                 key = f'{obj.__class__.__name__}.{obj.id}'
                 database[key] = obj
             return database
         else:
-            for key, value in models.classes.items():
+            for key, value in classes.items():
                 if key != 'BaseModel':
                     objs = self.__session.query(value).all()
                     if len(objs):
@@ -50,7 +50,7 @@ class DBStorage:
                             database[k] = obj
             return database
 
-    def new(self, obj):
+    def new(self, obj) -> None:
         '''Add object to DB'''
         self.__session.add(obj)
 
@@ -58,19 +58,19 @@ class DBStorage:
         '''Commit all current DB changes'''
         self.__session.commit()
 
-    def delete(self, obj=None):
+    def delete(self, obj=None) -> None:
         '''Delete current DB session'''
         if obj is None:
             return
         self.__session.delete(obj)
 
-    def reload(self):
+    def reload(self) -> None:
         '''Commit all current DB changes and session'''
         self.__session = Base.metadata.create_all(self.__engine)
         factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(factory)
         self.__session = Session()
 
-    def close(self):
+    def close(self) -> None:
         '''Remove private session attribute'''
         self.__session.close()
