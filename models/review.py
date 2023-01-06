ecb434d62c7f631b3da091b863120ed547ff1617
Jashon Osala <jashonosala@gmail.com>
2023-01-07 00:54:43 +0300

    Add foreign key in reviews Table

--- models/review.py
+++ models/review.py
@@ -2,7 +2,7 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from models.stringtemplates import HBNB_TYPE_STORAGE, DB
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, ForeignKey
 from os import getenv
 
 
@@ -11,8 +11,8 @@ class Review(BaseModel, Base):
     __tablename__ = 'reviews'
     if getenv(HBNB_TYPE_STORAGE) == DB:
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), nullable=False)
-        user_id = Column(String(60), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     else:
         place_id = ""
