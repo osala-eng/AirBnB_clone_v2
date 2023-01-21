0bd1ab35765b499ddae743bd822758f75d0d0b4c
Jashon Osala <jashonosala@gmail.com>
2023-01-21 23:33:21 +0300

    Install nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Script to set up web server for deployment
 
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+
 sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
 echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
 sudo ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
