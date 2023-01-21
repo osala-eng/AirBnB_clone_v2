8b615345725a06d22337ce99cda33e646cb6d523
Jashon Osala <jashonosala@gmail.com>
2023-01-21 23:31:22 +0300

    Create directories and change owner to ubuntu:ubuntu

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # Script to set up web server for deployment
 
-mkdir -p "/data/web_static/releases/test/"
-mkdir -p "/data/web_static/shared/"
-echo "<h1>Hello World!</h1>" > "/data/web_static/releases/test/index.html"
-ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
-chown -R ubuntu:ubuntu "/data/"
+sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
+echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
+sudo chown -hR ubuntu:ubuntu "/data/"
+
+
