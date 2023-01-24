41ad346b9fae783a68c43da523a32d8e757367e2
Jashon Osala <jashonosala@gmail.com>
2023-01-24 05:31:49 +0300

    Puppet config

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,19 +6,22 @@ $link = '/data/web_static/current'
 
 # Run apt-get update
 exec { 'apt-update':
-  command => '/usr/bin/apt-get update'
+  command => '/usr/bin/apt-get update',
+  user    => root,
 }
 
 # Install nginx
 package { 'nginx':
   ensure  => installed,
   require => Exec['apt-update'],
+  user    => root,
 }
 
 # Create directories
 exec { 'create-dirs':
   require => Package['nginx'],
   command => "/usr/bin/mkdir -p ${directory1} ${directory2}",
+  user    => root,
 }
 
 # Create index.html
@@ -26,30 +29,36 @@ file {'create-index.html':
   require => Exec['create-dirs'],
   path    => '/data/web_static/releases/test/index.html',
   content => '<h1>Hello this is puppet master!!!<h1/>',
+  user    => root,
 }
 
 # Create symbolic link
 exec {'symlink':
   require => File['create-index.html'],
-  command => "$/usr/bin/ln -sf ${directory1} ${link}"
+  command => "$/usr/bin/ln -sf ${directory1} ${link}",
+  user    => root,
 }
 
 # Change owner
 exec {'chown':
   require => Exec['symlink'],
   command => "$/usr/bin/chown -hR ubuntu:ubuntu /data/",
+  user    => root,
 }
 
 # Replace default site config
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  require => Package['nginx']
+  require => Package['nginx'],
+  user    => root,
 }-> exec { 'Replace config':
-  command => "/usr/bin/curl ${default_site} > ${default_site_loc}"
+  command => "/usr/bin/curl ${default_site} > ${default_site_loc}",
+  user    => root,
 }
 
 # Start nginx 
 service {'nginx':
   ensure  => running,
   require => Exec['Replace config'],
+  user    => root,
 }
